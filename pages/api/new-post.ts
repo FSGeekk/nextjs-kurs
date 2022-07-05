e3ea764bda943a445d4f9457d1772980657e4c56
Feridum <aleksander.patschek@gmail.com>
2022-07-05 17:12:16 +0200

    save data to database

---
+++ pages/api/new-post.ts
@@ -0,0 +1,31 @@
+import { Prisma } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from '../../lib/prisma';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const body = req.body;
+
+  try {
+    const post = await prisma.post.create({
+      data: {
+        ...body,
+        createdAt: new Date(),
+      },
+    });
+
+    return res.status(200).json({ post });
+  } catch (e) {
+    console.log(e);
+    if (e instanceof Prisma.PrismaClientValidationError) {
+      return res.status(400).json({ error: e.message });
+    }
+
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      if (e.code === 'P2002') {
+        return res.status(400).json({ error: 'Taki slug już istnieje w systemie' });
+      }
+    }
+  }
+
+  return res.status(400).json({ error: 'Something went wrong. Try again' });
+}
--- pages/new-post.tsx
+++ pages/new-post.tsx
@@ -1,41 +1,61 @@
-import { AppShell, Button, Center, Container, Header, Space, Title } from '@mantine/core';
+import { Alert, AppShell, Button, Center, Container, Header, Space, Title } from '@mantine/core';
 import { Post as PostPrisma } from '@prisma/client';
 import type { NextPage } from 'next';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
+
 import { TextInput } from '../components/form/TextInput';
 import { TextareaInput } from '../components/form/TextareaInput';
-
-const schema = yup
-  .object({
-    title: yup.string().required('Tytuł jest wymagany'),
-    content: yup.string().required('Treść jest wymagana'),
-    slug: yup.string().required('Slug jest wymagany'),
-  })
-  .required();
-
-interface PostForm extends yup.InferType<typeof schema> {}
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { PostSchema, postSchema } from '../utils/postSchema';
 
 const NewPost: NextPage = () => {
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<PostForm>({
-    resolver: yupResolver(schema),
+  const router = useRouter();
+  const [error, setError] = useState<null | string>(null);
+  const { control, handleSubmit } = useForm<PostSchema>({
+    resolver: yupResolver(postSchema),
+    defaultValues: {
+      title: '',
+      content: '',
+      slug: '',
+    },
   });
 
-  const onSubmit = (data: PostForm) => console.log(data);
+  const onSubmit = async (data: PostSchema) => {
+    setError(null);
+    const response = await fetch('/api/new-post', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    console.log(response);
+    if (response.status === 200) {
+      return router.push('/');
+    }
+
+    const content = await response.json();
+    setError(content.error);
+  };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <TextInput control={control} name="title" label="Tytuł" />
-      <TextareaInput control={control} name="content" autosize minRows={10} maxRows={20} label="Treść" />
-      <TextInput control={control} name="slug" label="Slug" />
-      <Space h="xl" />
-      <Button type="submit"> Dodaj wpis</Button>
-    </form>
+    <>
+      {error && (
+        <Alert title="Błąd!" color="red" withCloseButton onClose={() => setError(null)}>
+          {error}
+        </Alert>
+      )}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextInput control={control} name="title" label="Tytuł" />
+        <TextareaInput control={control} name="content" autosize minRows={10} maxRows={20} label="Treść" />
+        <TextInput control={control} name="slug" label="Slug" />
+        <Space h="xl" />
+        <Button type="submit"> Dodaj wpis</Button>
+      </form>
+    </>
   );
 };
 
